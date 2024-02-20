5d566e5ca9e404be7b2f53b323cff58c202e79ae
Aaron <rainbow.ept.ly@gmail.com>
2024-02-20 17:34:39 -0500

    updated
    updated

--- src/ProfileInfo.js
+++ src/ProfileInfo.js
@@ -1,23 +1,11 @@
 import React from "react";
-import Card from "react-bootstrap/Card";
 
 export const ProfileInfo = () => {
   return (
     <div style={{ margin: "10px" }}>
-      <Card
-        style={{ minWidth: "200px", minHeight: "400px", marginTop: "20px" }}
-      >
-        <Card.Title
-          style={{ color: "grey", fontSize: "16px", marginLeft: "10px" }}
-        ></Card.Title>
-        <Card.Body>
-          <div style={{ textAlign: "center" }}>
-            {/* <img src="/photo.jpg" alt="My Image" width="100" height="100"></img> */}
-          </div>
-
-          <p style={{ fontWeight: "bold", textAlign: "center" }}>Photo</p>
-        </Card.Body>
-      </Card>
+      <p style={{ fontWeight: "bold", textAlign: "center" }}>
+        <img src="/photo.jpg" alt="My Image" width="400" height="400"></img>
+      </p>
     </div>
   );
 };
